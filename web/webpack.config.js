3e2015b13915de0c478041a29f370d3742887726
Sean Stangl <sean.stangl@gmail.com>
2017-09-03 13:56:22 -0700

    Add the minify-constant-folding plugin to the webpack build.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015']
+          presets: ['es2015'],
+          plugins: ['minify-constant-folding']
         }
       }
     ]
