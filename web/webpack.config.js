3412cfe653fe4cdded5a624682608eb38df7958f
Sean Stangl <sean.stangl@gmail.com>
2017-10-31 06:53:41 -0700

    Generate all client-side non-HTML into build/static/.
    This facilitates moving to a dynamic server.

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
 
   output: {
-    path: path.resolve(__dirname, "build/scripts"),
+    path: path.resolve(__dirname, "build/static/scripts"),
     filename: "[name].bundle.js",
   },
 
