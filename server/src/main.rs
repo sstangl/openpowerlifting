5436710a09499f46f28433a02825e0f18582b5c4
Michael Vessia <michaelvessia@gmail.com>
2018-03-08 23:02:39 -0500

    Add the data page with same content as prd site. (#899)
    TODO: file sizes for downloads

--- server/src/main.rs
+++ server/src/main.rs
@@ -147,6 +147,19 @@ fn status(
     Some(Template::render("status", &context))
 }
 
+#[get("/data")]
+fn data(
+    opldb: State<opldb::OplDb>,
+    langinfo: State<langpack::LangInfo>,
+    languages: AcceptLanguage,
+    cookies: Cookies,
+) -> Option<Template> {
+    let lang = select_display_language(languages, &cookies);
+
+    let context = pages::data::Context::new(&opldb, lang, &langinfo);
+    Some(Template::render("data", &context))
+}
+
 #[get("/")]
 fn index() -> Redirect {
     Redirect::to("/u/kristyhawkins")
@@ -157,7 +170,7 @@ fn rocket(opldb: opldb::OplDb, langinfo: langpack::LangInfo) -> rocket::Rocket {
     rocket::ignite()
         .manage(opldb)
         .manage(langinfo)
-        .mount("/", routes![index, lifter, meet, statics, status])
+        .mount("/", routes![index, lifter, meet, statics, status, data])
         .attach(Template::fairing())
 }
 
