2d9d1dcbcbde5156cf6d2f5cdfc66771fbd8af33
mike <michaelvessia@gmail.com>
2018-03-08 23:42:18 -0500

    Cargo fmt change

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -2,12 +2,12 @@
 
 // External dependencies.
 extern crate csv;
+#[macro_use]
+extern crate enum_iterator_derive;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
-#[macro_use]
-extern crate enum_iterator_derive;
 
 // Exported modules.
 pub mod langpack;
