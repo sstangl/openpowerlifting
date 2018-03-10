986717960cae4d029870e81d2edf24e5630da15c
mike <michaelvessia@gmail.com>
2018-03-09 22:03:53 -0500

    Track faq and contact pages

---
+++ server/src/pages/faq.rs
@@ -0,0 +1,24 @@
+//! Logic for the faq page.
+
+use langpack::{self, Language};
+
+/// The context object passed to `templates/faq.tera`
+#[derive(Serialize)]
+pub struct Context<'a> {
+    pub page_title: &'a str,
+    pub language: Language,
+    pub strings: &'a langpack::Translations,
+}
+
+impl<'a> Context<'a> {
+    pub fn new(language: Language, langinfo: &'a langpack::LangInfo) -> Context<'a> {
+        let strings = langinfo.get_translations(language);
+        let page_title = &strings.header.faq;
+
+        Context {
+            page_title: page_title,
+            strings: strings,
+            language: language,
+        }
+    }
+}
