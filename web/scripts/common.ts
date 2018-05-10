44812e2858a86bc9965c5cb374cf6f0dfd409969
Sean Stangl <sean.stangl@gmail.com>
2018-05-09 19:11:47 -0700

    Change Garrett Fear's name to purple. Closes #1181.

--- web/scripts/common.ts
+++ web/scripts/common.ts
@@ -119,7 +119,7 @@ export function makeRowObj(row, index?: number): RowObject {
     if (fullname === "Ben Gianacakos") {
         // Pink.
         name = '<a style="text-decoration-color: #FF80AB;" href="' + this.makeLiftersUrl(fullname) + '"><span style="color: #FF80AB;">' + fullname + '</span></a>';
-    } else if (fullname === "Jo Whiteley") {
+    } else if (fullname === "Jo Whiteley" || fullname === "Garrett Fear") {
         // Purple.
         name = '<a style="text-decoration-color: #AE76FF;" href="' + this.makeLiftersUrl(fullname) + '"><span style="color: #AE76FF;">' + fullname + '</span></a>';
     } else if (fullname === "Kristy Hawkins" || fullname === "Amanda Kohatsu" || fullname === "Joe Sullivan" || fullname === "Sarah Wellberg") {
