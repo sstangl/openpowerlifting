87c8af021063c67419b297185027a0f827c4c57e
Sean Stangl <sean.stangl@gmail.com>
2018-11-03 12:01:09 -0700

    Define opltypes::Date::age_on(), adding many doctests, tests, and comments.

--- modules/opltypes/src/date.rs
+++ modules/opltypes/src/date.rs
@@ -7,6 +7,8 @@ use std::fmt;
 use std::num;
 use std::str::FromStr;
 
+use Age;
+
 /// Our data uses imprecise dates in the "YYYY-MM-DD" format,
 /// with no timezone or time data.
 /// Dates in this format can be stored as a `u32` with value YYYYMMDD.
@@ -15,24 +17,122 @@ use std::str::FromStr;
 pub struct Date(u32);
 
 impl Date {
+    /// Creates a Date object from its exact internal representation.
+    ///
+    /// FIXME: Using this constructor bypasses error checks.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use opltypes::Date;
+    /// let date = Date::from_u32(1988_02_16);
+    /// assert_eq!(date.year(), 1988);
+    /// assert_eq!(date.month(), 2);
+    /// assert_eq!(date.day(), 16);
+    /// ```
     #[inline]
     pub fn from_u32(u: u32) -> Date {
         Date(u)
     }
 
+    /// Returns the year as an integer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use opltypes::Date;
+    /// let date = "1988-02-16".parse::<Date>().unwrap();
+    /// assert_eq!(date.year(), 1988);
+    /// ```
     #[inline]
     pub fn year(self) -> u32 {
         self.0 / 10_000
     }
 
+    /// Returns the month as an integer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use opltypes::Date;
+    /// let date = "1988-02-16".parse::<Date>().unwrap();
+    /// assert_eq!(date.month(), 2);
+    /// ```
     #[inline]
     pub fn month(self) -> u32 {
         (self.0 / 100) % 100
     }
 
+    /// Returns the day as an integer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use opltypes::Date;
+    /// let date = "1988-02-16".parse::<Date>().unwrap();
+    /// assert_eq!(date.day(), 16);
+    /// ```
     #[inline]
     pub fn day(self) -> u32 {
         self.0 % 100
+    }
+
+    /// Returns the month and day as a combined integer.
+    ///
+    /// This is useful mostly for age calculations, where the `monthday()`
+    /// corresponds to an exact day in the given year.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use opltypes::Date;
+    /// let date = "1988-02-16".parse::<Date>().unwrap();
+    /// assert_eq!(date.monthday(), 0216);
+    /// ```
+    #[inline]
+    pub fn monthday(self) -> u32 {
+        (self.0 % 10000)
+    }
+
+    /// Calculates the Age of a lifter on a given date,
+    /// where `self` is the lifter's BirthDate.
+    ///
+    /// # Failures
+    ///
+    /// Fails if the lifter was not yet born by the given date.
+    ///
+    /// Fails if the lifter would be more than 256 years old.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use opltypes::{Age,Date};
+    /// let birthdate = "1991-12-16".parse::<Date>().unwrap();
+    /// let meetdate  = "2018-11-03".parse::<Date>().unwrap();
+    /// assert_eq!(birthdate.age_on(meetdate), Ok(Age::Exact(26)));
+    /// ```
+    pub fn age_on(self, date: Date) -> Result<Age, &'static str> {
+        // The date of comparison must be after the lifter was born.
+        if date.0 < self.0 {
+            return Err("Lifter was not born yet");
+        }
+
+        // The diff of years must be able to fit into the limited range of an Age.
+        let years_u32: u32 = date.year() - self.year();
+        if years_u32 > u8::max_value().into() {
+            return Err("Calculated Age greater than 256");
+        }
+        let years = years_u32 as u8;
+
+        // If their birthday occured in the most recent year, just diff years.
+        if date.monthday() >= self.monthday() {
+            Ok(Age::Exact(years))
+        } else {
+            // This subtraction cannot underflow: the case for the lifter
+            // not being born yet was handled above; since the lifter was born,
+            // if `years == 0`, then `date.monthday() >= self.monthday()`.
+            Ok(Age::Exact(years - 1))
+        }
     }
 }
 
@@ -180,5 +280,47 @@ mod test {
     fn test_date_display() {
         let date = "2017-03-04".parse::<Date>().unwrap();
         assert_eq!(format!("{}", date), "2017-03-04");
+    }
+
+    #[test]
+    fn test_age_on() {
+        // The reference birthdate used in all the tests below.
+        let birthdate = "1988-02-16".parse::<Date>().unwrap();
+
+        // Not born yet, obvious by year.
+        let date = "1987-01-01".parse::<Date>().unwrap();
+        assert!(birthdate.age_on(date).is_err());
+
+        // Not born yet, but in the same year.
+        let date = "1988-02-15".parse::<Date>().unwrap();
+        assert!(birthdate.age_on(date).is_err());
+
+        // Exact date of birth.
+        let date = "1988-02-16".parse::<Date>().unwrap();
+        assert_eq!(birthdate.age_on(date).unwrap(), Age::Exact(0));
+
+        // The next day.
+        let date = "1988-02-16".parse::<Date>().unwrap();
+        assert_eq!(birthdate.age_on(date).unwrap(), Age::Exact(0));
+
+        // The next year, but not yet to 1 years old.
+        let date = "1989-02-15".parse::<Date>().unwrap();
+        assert_eq!(birthdate.age_on(date).unwrap(), Age::Exact(0));
+
+        // One years old on the day.
+        let date = "1989-02-16".parse::<Date>().unwrap();
+        assert_eq!(birthdate.age_on(date).unwrap(), Age::Exact(1));
+
+        // A date in the future, before the monthday of birth.
+        let date = "2018-01-04".parse::<Date>().unwrap();
+        assert_eq!(birthdate.age_on(date).unwrap(), Age::Exact(29));
+
+        // A date in the future, after the monthday of birth.
+        let date = "2018-11-03".parse::<Date>().unwrap();
+        assert_eq!(birthdate.age_on(date).unwrap(), Age::Exact(30));
+
+        // A date so far in the future that Age would be >256.
+        let date = "3018-11-03".parse::<Date>().unwrap();
+        assert!(birthdate.age_on(date).is_err());
     }
 }
