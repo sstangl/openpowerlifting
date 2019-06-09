82c3e23db3d3aebcc287be388f32cdccbf338e88
Sean Stangl <sean.stangl@gmail.com>
2019-06-09 08:31:51 -0700

    Delete all CPU scripts. They changed the database format.

---
+++ meet-data/cpu/cpu-hash-name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#
+# Calculates the unique folder ID for a given meet.
+
+import zlib
+
+# `date` is a ISO8601 datestring like "2019-01-16".
+# `name` is the name of the competition.
+def meethash(date, name):
+    assert len(date) == 10
+    namehash = format(zlib.crc32(bytes(name, 'utf-8')), '08x')
+    assert len(namehash) == 8
+    return "%s-%s" % (date, namehash)
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) != 3:
+        print(" Usage: %s <MeetDate> <MeetName>")
+        sys.exit(1)
+
+    print(meethash(sys.argv[1], sys.argv[2]))
