102642eeb18b8ae03a9ba9178308ab369f56dca5
Sean Stangl <sean.stangl@gmail.com>
2019-09-23 21:05:12 -0600

    Add a SocialBot. It isn't hosted anywhere yet. Closes #3867

---
+++ server/socialbot/parse-instagram.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Parses the OpenPowerlifting account on Instagram for follower counts.
+
+import datetime
+import urllib.request
+
+URL = "https://www.instagram.com/openpowerlifting/"
+
+
+def get_html(url):
+    with open("test.html") as f:
+        return f.read()
+
+    with urllib.request.urlopen(url) as fp:
+        html = fp.read().decode("utf-8")
+    return html
+
+
+def get_followers(html):
+    target = '"edge_followed_by":{"count":'
+    start = html.index(target)
+    end = html.index('}', start)
+    return int(html[start:end][len(target):])
+
+
+def get_date():
+    today = datetime.date.today()
+    return str(today)
+
+
+def main():
+    html = get_html(URL)
+    followers = get_followers(html)
+    date = get_date()
+    print(date + ": **" + '{:,}'.format(followers) + "** graph enthusiasts")
+
+
+if __name__ == "__main__":
+    main()
