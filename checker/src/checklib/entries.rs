0a4e8728397c685eefdedf758f047c4d20e2494c
mikebeelen <mikebeelen111@gmail.com>
2019-12-29 18:56:02 +0100

    Add 6 NPB Bench Meets from wimwam.nl

--- checker/src/checklib/entries.rs
+++ checker/src/checklib/entries.rs
@@ -432,7 +432,7 @@ fn check_column_name(name: &str, line: u64, report: &mut Report) -> String {
                 // Common short words that mostly translate to "the".
                 "bin" | "da" | "de" | "do" | "del" | "den" | "der" | "des" | "di"
                 | "dos" | "du" | "e" | "el" | "in't" | "la" | "le" | "los" | "'t"
-                | "v" | "v." | "v.d." | "van" | "von" | "zur" => {
+                | "ten" | "v" | "v." | "v.d." | "van" | "von" | "zur" => {
                     continue;
                 }
 
