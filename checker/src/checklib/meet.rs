5402b2b87ed125fb659c48a18387642e35daf4d9
Sean Stangl <sean.stangl@gmail.com>
2020-02-22 08:17:00 -0700

    Add 2020 FIPL 1° Campionato Italiano Sub-Junior Junior e Master di Stacco Classic

--- checker/src/checklib/meet.rs
+++ checker/src/checklib/meet.rs
@@ -247,7 +247,7 @@ pub fn check_meetname(
 
     for c in s.chars() {
         // Non-ASCII characters are allowed.
-        if !c.is_alphanumeric() && !" -&.'/:".contains(c) {
+        if !c.is_alphanumeric() && !" -&.'/°:".contains(c) {
             report.error(format!("Illegal character in MeetName '{}'", s));
             break;
         }
