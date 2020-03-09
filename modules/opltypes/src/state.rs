149199f5b272c49297652952451d31f5ee4e8b47
Sean Stangl <sean.stangl@gmail.com>
2020-03-08 19:42:40 -0600

    Add 2020 WABDL Supino e Levantamento Terra A Caverna Gym. Closes #5067

--- modules/opltypes/src/state.rs
+++ modules/opltypes/src/state.rs
@@ -258,11 +258,62 @@ pub enum AustraliaState {
 }
 
 /// A state in Brazil.
-#[rustfmt::skip]
 #[derive(Copy, Clone, Debug, EnumString, PartialEq, Serialize, ToString)]
 pub enum BrazilState {
-    AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA,
-    PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO
+    /// Acre.
+    AC,
+    /// Alagoas.
+    AL,
+    /// Amapá.
+    AP,
+    /// Amazonas.
+    AM,
+    /// Bahia.
+    BA,
+    /// Ceará.
+    CE,
+    /// Distrito Federal.
+    DF,
+    /// Espírito Santo.
+    ES,
+    /// Goiás.
+    GO,
+    /// Maranhão.
+    MA,
+    /// Mato Grosso.
+    MT,
+    /// Mato Grosso do Sul.
+    MS,
+    /// Minas Gerais.
+    MG,
+    /// Pará.
+    PA,
+    /// Paraíba.
+    PB,
+    /// Paraná.
+    PR,
+    /// Pernambuco.
+    PE,
+    /// Piauí.
+    PI,
+    /// Rio de Janeiro.
+    RJ,
+    /// Rio Grande do Norte.
+    RN,
+    /// Rio Grande do Sul.
+    RS,
+    /// Rondônia.
+    RO,
+    /// Roraima.
+    RR,
+    /// Santa Catarina.
+    SC,
+    /// São Paulo.
+    SP,
+    /// Sergipe.
+    SE,
+    /// Tocantins.
+    TO,
 }
 
 /// A state in Canada.
