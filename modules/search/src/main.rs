a66b44b49311bc8a2f9582c458211ff328430996
Sean Stangl <sean.stangl@gmail.com>
2020-06-24 22:26:10 -0600

    Add a CLI driver for modules/search
    Currently just boilerplate. The plan is that as search features are
    added, we'll build them up in the CLI version before worrying about full
    presentation on the website.

---
+++ modules/search/src/main.rs
@@ -0,0 +1,33 @@
+//! CLI driver for developing the search interface.
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+const READLINE_PROMPT: &str = ">>> ";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const LIFTERS_CSV: &str = "../../build/lifters.csv";
+    const MEETS_CSV: &str = "../../build/meets.csv";
+    const ENTRIES_CSV: &str = "../../build/entries.csv";
+
+    let _db = opldb::OplDb::from_csv(LIFTERS_CSV, MEETS_CSV, ENTRIES_CSV)?;
+
+    let mut rl = Editor::<()>::new();
+    loop {
+        match rl.readline(READLINE_PROMPT) {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                println!("{}", line);
+            }
+            Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    std::process::exit(0); // Dropping the database takes a while.
+}
