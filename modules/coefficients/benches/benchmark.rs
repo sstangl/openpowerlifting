9f1c1d419220641514f101bc112eac09e61155a9
Sean Stangl <sean.stangl@gmail.com>
2021-01-24 11:52:50 -0700

    Convert benchmarks to Criterion. Closes #6749
    Criterion runs on Stable Rust: this is preparatory work anticipating the
    next release of Rocket.

--- modules/coefficients/benches/benchmark.rs
+++ modules/coefficients/benches/benchmark.rs
@@ -1,16 +1,10 @@
 //! Simple benchmarks to assess the speed of coefficient calculation.
 
-#![feature(test)]
-
-extern crate coefficients;
-extern crate opltypes;
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
 use opltypes::*;
 
-mod benches {
-    use super::*;
-
-    extern crate test;
-    use self::test::Bencher;
+pub fn coefficient_benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("coefficients");
 
     // Constants used in the benchmarks below, to keep them all the same.
     const BODYWEIGHT_MEN: WeightKg = WeightKg::from_i32(93);
@@ -19,105 +13,160 @@ mod benches {
     const BODYWEIGHT_WOMEN: WeightKg = WeightKg::from_i32(63);
     const TOTAL_WOMEN: WeightKg = WeightKg::from_i32(475);
 
-    /// Benchmarks AH.
-    #[bench]
-    fn ah(b: &mut Bencher) {
-        b.iter(|| {
-            coefficients::ah(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+    const AGE: Age = Age::Exact(55);
 
-    /// Benchmarks Dots.
-    #[bench]
-    fn dots(b: &mut Bencher) {
-        b.iter(|| {
-            coefficients::wilks2020(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+    // Each test benchmarks the calculation for a single entry.
+    group.throughput(Throughput::Elements(1));
 
-    /// Benchmarks Glossbrenner for men. Behaves differently than for women.
-    #[bench]
-    fn glossbrenner_men(b: &mut Bencher) {
+    // Benchmarks AH.
+    group.bench_function("ah", |b| {
         b.iter(|| {
-            coefficients::glossbrenner(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::ah(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
 
-    /// Benchmarks Glossbrenner for women. Behaves differently than for men.
-    #[bench]
-    fn glossbrenner_women(b: &mut Bencher) {
+    // Benchmarks Dots.
+    group.bench_function("dots", |b| {
         b.iter(|| {
-            coefficients::glossbrenner(Sex::F, BODYWEIGHT_WOMEN, TOTAL_WOMEN);
-        })
-    }
+            coefficients::dots(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
 
-    /// Benchmarks Hoffman.
-    #[bench]
-    fn hoffman(b: &mut Bencher) {
+    // Benchmarks Glossbrenner for men and women, since they have different implementations.
+    group.bench_function("glossbrenner men", |b| {
         b.iter(|| {
-            coefficients::hoffman(BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::glossbrenner(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
+    group.bench_function("glossbrenner women", |b| {
+        b.iter(|| {
+            coefficients::glossbrenner(
+                black_box(Sex::F),
+                black_box(BODYWEIGHT_WOMEN),
+                black_box(TOTAL_WOMEN),
+            );
+        });
+    });
 
-    /// Benchmarks IPF points.
-    #[bench]
-    fn ipf(b: &mut Bencher) {
+    // Benchmarks IPF Goodlift Points.
+    group.bench_function("goodlift", |b| {
+        b.iter(|| {
+            coefficients::goodlift(
+                black_box(Sex::M),
+                black_box(Equipment::Raw),
+                black_box(Event::sbd()),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
+
+    // Benchmarks Hoffman.
+    group.bench_function("hoffman", |b| {
+        b.iter(|| {
+            coefficients::hoffman(black_box(BODYWEIGHT_MEN), black_box(TOTAL_MEN));
+        });
+    });
+
+    // Benchmarks IPF Points.
+    group.bench_function("ipf", |b| {
         b.iter(|| {
             coefficients::ipf(
-                Sex::M,
-                Equipment::Raw,
-                Event::sbd(),
-                BODYWEIGHT_MEN,
-                TOTAL_MEN,
+                black_box(Sex::M),
+                black_box(Equipment::Raw),
+                black_box(Event::sbd()),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
             );
-        })
-    }
+        });
+    });
 
-    /// Benchmarks NASA.
-    #[bench]
-    fn nasa(b: &mut Bencher) {
+    // Benchmarks McCulloch.
+    group.bench_function("mcculloch", |b| {
         b.iter(|| {
-            coefficients::hoffman(BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::mcculloch(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+                black_box(AGE),
+            );
+        });
+    });
 
-    /// Benchmarks Reshel.
-    #[bench]
-    fn reshel(b: &mut Bencher) {
+    // Benchmarks NASA Points.
+    group.bench_function("nasa", |b| {
         b.iter(|| {
-            coefficients::reshel(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::nasa(black_box(BODYWEIGHT_MEN), black_box(TOTAL_MEN));
+        });
+    });
 
-    /// Benchmarks Schwartz/Malone for men.
-    #[bench]
-    fn schwartzmalone_men(b: &mut Bencher) {
+    // Benchmarks Reshel.
+    group.bench_function("reshel", |b| {
         b.iter(|| {
-            coefficients::schwartzmalone(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::reshel(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
 
-    /// Benchmarks Schwartz/Malone for women.
-    #[bench]
-    fn schwartzmalone_women(b: &mut Bencher) {
+    // Benchmarks Schwartz/Malone for men and women, since they have different implementations.
+    group.bench_function("schwartzmalone men", |b| {
         b.iter(|| {
-            coefficients::schwartzmalone(Sex::F, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::schwartzmalone(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
+    group.bench_function("schwartzmalone women", |b| {
+        b.iter(|| {
+            coefficients::schwartzmalone(
+                black_box(Sex::F),
+                black_box(BODYWEIGHT_WOMEN),
+                black_box(TOTAL_WOMEN),
+            );
+        });
+    });
 
-    /// Benchmarks Wilks.
-    #[bench]
-    fn wilks(b: &mut Bencher) {
+    // Benchmarks Wilks.
+    group.bench_function("wilks", |b| {
         b.iter(|| {
-            coefficients::wilks(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::wilks(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
 
-    /// Benchmarks Wilks2020.
-    #[bench]
-    fn wilks2020(b: &mut Bencher) {
+    // Benchmarks Wilks2020.
+    group.bench_function("wilks2020", |b| {
         b.iter(|| {
-            coefficients::wilks2020(Sex::M, BODYWEIGHT_MEN, TOTAL_MEN);
-        })
-    }
+            coefficients::wilks2020(
+                black_box(Sex::M),
+                black_box(BODYWEIGHT_MEN),
+                black_box(TOTAL_MEN),
+            );
+        });
+    });
+
+    group.finish();
 }
+
+criterion_group!(benches, coefficient_benchmarks);
+criterion_main!(benches);
--- modules/opltypes/benches/username.rs
+++ modules/opltypes/benches/username.rs
@@ -1,44 +1,43 @@
 //! Simple benchmarks to assess the speed of username generation.
 
-#![feature(test)]
+use criterion::{black_box, criterion_group, criterion_main, Criterion, Throughput};
+use opltypes::Username;
 
-extern crate opltypes;
+pub fn username_benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("username");
 
-mod benches {
-    use super::*;
-
-    extern crate test;
-    use self::test::Bencher;
-
-    use opltypes::Username;
-
-    /// Tests the all-ASCII fast path (50 ASCII characters).
-    #[bench]
-    fn make_username_ascii(b: &mut Bencher) {
-        let ascii_name = "1234567890".repeat(5);
+    // Tests the all-ASCII fast path (50 ASCII characters).
+    let ascii_name = "1234567890".repeat(5);
+    group.throughput(Throughput::Elements(ascii_name.len() as u64));
+    group.bench_("ascii", |b| {
         b.iter(|| {
-            Username::from_name(&ascii_name).unwrap();
+            Username::from_name(black_box(&ascii_name)).unwrap();
         });
-    }
+    });
 
-    /// Tests the UTF-8 path (50 UTF-8 characters).
-    ///
-    /// The string that's chosen is one where a single UTF-8 character
-    /// expands into two ASCII characters, doubling the resultant string size.
-    #[bench]
-    fn make_username_utf8_expansion(b: &mut Bencher) {
-        let utf8_name = "þ".repeat(50);
+    // Tests the UTF-8 path (50 UTF-8 characters).
+    //
+    // The string that's chosen is one where a single UTF-8 character
+    // expands into two ASCII characters, doubling the resultant string size.
+    let utf8_name = "þ".repeat(50);
+    group.throughput(Throughput::Elements(utf8_name.len() as u64));
+    group.bench_function("utf8", |b| {
         b.iter(|| {
-            Username::from_name(&utf8_name).unwrap();
+            Username::from_name(black_box(&utf8_name)).unwrap();
         });
-    }
+    });
 
-    /// Tests Japanese names written in Hiragana, which get normalized into Katakana.
-    #[bench]
-    fn make_username_hiragana(b: &mut Bencher) {
-        let hiragana_name = "なべやかん".repeat(10);
+    // Tests Japanese names written in Hiragana, which get normalized into Katakana.
+    let hiragana_name = "なべやかん".repeat(10);
+    group.throughput(Throughput::Elements(hiragana_name.len() as u64));
+    group.bench_function("hiragana", |b| {
         b.iter(|| {
-            Username::from_name(&hiragana_name).unwrap();
+            Username::from_name(black_box(&hiragana_name)).unwrap();
         });
-    }
+    });
+
+    group.finish();
 }
+
+criterion_group!(benches, username_benchmarks);
+criterion_main!(benches);
