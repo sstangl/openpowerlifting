8b73471acc12ec4afe9a9552156c553a0b5cfae1
Sean Stangl <sean.stangl@gmail.com>
2021-02-12 23:02:55 -0700

    Add modules/db/benches for benchmarking database loading time
    On my desktop:
     time:   [12.138 s 12.182 s 12.229 s]
     thrpt:  [192.11 Kelem/s 192.86 Kelem/s 193.56 Kelem/s]
    
    I'm going to try some things to improve this while keeping with CSVs.

---
+++ modules/db/benches/loading.rs
@@ -0,0 +1,35 @@
+//! Benchmarks loading the database from data files.
+
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use opldb::OplDb;
+
+const LIFTERS_CSV: &str = "../../build/lifters.csv";
+const MEETS_CSV: &str = "../../build/meets.csv";
+const ENTRIES_CSV: &str = "../../build/entries.csv";
+
+/// Counts the number of lines in a file, for establishing throughput.
+fn count_lines_in(path: &str) -> usize {
+    use std::io::BufRead;
+    let file = std::fs::File::open(path).expect("a readable file");
+    let reader = std::io::BufReader::new(file);
+    reader.lines().count()
+}
+
+pub fn loading_benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("loading");
+    let num_entries = count_lines_in(ENTRIES_CSV);
+
+    // Loading CSV files takes forever, so collect the minimum number of samples.
+    // Otherwise, the benchmark takes something like 20 minutes to run.
+    group.sample_size(10);
+
+    group.throughput(Throughput::Elements(num_entries as u64));
+    group.bench_function("csv", |b| {
+        b.iter(|| {
+            OplDb::from_csv(LIFTERS_CSV, MEETS_CSV, ENTRIES_CSV).unwrap();
+        });
+    });
+}
+
+criterion_group!(benches, loading_benchmarks);
+criterion_main!(benches);
