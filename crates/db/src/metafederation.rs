ef69a8baf88c964075e9d5263542c31433c24bff
Jo Whiteley <jowhiteley1@gmail.com>
2022-04-19 09:22:07 +0000

    Add EPA to ManxPL metafed

--- crates/db/src/metafederation.rs
+++ crates/db/src/metafederation.rs
@@ -798,7 +798,7 @@ impl MetaFederation {
             MetaFederation::LPF => affiliation!(meet, entry, LPF, IPF, EPF),
             MetaFederation::KPF => affiliation!(meet, entry, KPF, IPF, AsianPF),
             MetaFederation::KRAFT => affiliation!(meet, entry, KRAFT, IPF, EPF, NordicPF),
-            MetaFederation::ManxPL => affiliation!(meet, entry, ManxPL, IPF, EPF, BP),
+            MetaFederation::ManxPL => affiliation!(meet, entry, ManxPL, IPF, EPF, BP, EPA),
             MetaFederation::NauruPF => affiliation!(meet, entry, NauruPF, IPF, ORPF),
             MetaFederation::NIPF => affiliation!(meet, entry, NIPF, IPF, EPF, BP),
             MetaFederation::NSF => affiliation!(meet, entry, NSF, IPF, EPF, NordicPF),
