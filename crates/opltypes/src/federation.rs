ee659e948d230e0dede2b33b3ea19aeff1d5cfda
Sean Stangl <sean.stangl@gmail.com>
2022-05-06 22:26:14 -0600

    Fix build: definition of GPC-Scotland

--- crates/opltypes/src/federation.rs
+++ crates/opltypes/src/federation.rs
@@ -505,7 +505,7 @@ pub enum Federation {
     /// Scotland branch of the GPC.
     #[serde(rename = "GPC-Scotland")]
     #[strum(to_string = "GPC-Scotland", serialize = "gpc-scotland")]
-    GPCSCOTLAND,
+    GPCScotland,
 
     /// Ukrainian branch of the GPC.
     #[serde(rename = "GPC-UKR")]
@@ -1712,7 +1712,7 @@ impl Federation {
             Federation::GPCLAT => false,
             Federation::GPCNZ => false,
             Federation::GPCPOL => false,
-            Federation::GPCSCOTLAND => false,
+            Federation::GPCScotland => false,
             Federation::GPCUKR => false,
             Federation::GPCUSA => false,
             Federation::GPCRUS => false,
@@ -2081,7 +2081,7 @@ impl Federation {
             Federation::GPCLAT => Some(Country::Latvia),
             Federation::GPCNZ => Some(Country::NewZealand),
             Federation::GPCPOL => Some(Country::Poland),
-            Federation::GPCSCOTLAND => Some(Country::Scotland),
+            Federation::GPCScotland => Some(Country::Scotland),
             Federation::GPCUKR => Some(Country::Ukraine),
             Federation::GPCUSA => Some(Country::USA),
             Federation::GPCRUS => Some(Country::Russia),
@@ -2482,14 +2482,7 @@ impl Federation {
             Federation::GPCLAT => Some(Federation::GPC),
             Federation::GPCNZ => Some(Federation::GPC),
             Federation::GPCPOL => Some(Federation::GPC),
-            Federation::GPCSCOTLAND => Some(Federation::GPC),
-            // GPC-Scotland has been affiliated since 2022.
-                if date.year() >= 2022 {
-                    Some(Federation::GPC)
-                } else {
-                    None
-                }
-            }
+            Federation::GPCScotland => Some(Federation::GPC),
             Federation::GPCUKR => Some(Federation::GPC),
             Federation::GPCUSA => Some(Federation::GPC),
             Federation::GPCRUS => Some(Federation::GPC),
@@ -2950,7 +2943,7 @@ impl Federation {
             Federation::GPCLAT => PointsSystem::Glossbrenner,
             Federation::GPCNZ => PointsSystem::Glossbrenner,
             Federation::GPCPOL => PointsSystem::Glossbrenner,
-            Federation::GPCSCOTLAND => PointsSystem::Reshel,
+            Federation::GPCScotland => PointsSystem::Reshel,
             Federation::GPCUKR => PointsSystem::Glossbrenner,
             Federation::GPCUSA => PointsSystem::Glossbrenner,
             Federation::GPCRUS => PointsSystem::Glossbrenner,
