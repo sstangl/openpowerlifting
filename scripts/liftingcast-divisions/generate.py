ec4abd20d86974a5352dce854a8f4d0632042302
James Wakefield <jmw1982@gmail.com>
2023-10-10 22:15:46 +1100

    Script for generating LiftingCast division import CSVs

---
+++ scripts/liftingcast-divisions/generate.py
@@ -0,0 +1,70 @@
+import json
+import sys
+import csv
+
+from pathlib import Path
+from csv import DictWriter
+
+
+def lifts_str(event):
+    lifts_l = []
+    if "S" in event:
+        lifts_l.append("squat")
+    if "B" in event:
+        lifts_l.append("bench")
+    if "D" in event:
+        lifts_l.append("dead")
+    return ",".join(lifts_l)
+
+def gen_divisions(config_d):
+    for sex in ["M", "F"]:
+        for age in config_d["age"]:
+            for eq_d in config_d["equipment"]:
+                for event in config_d["events"]:
+                    if sex == "M":
+                        wtcls_l = config_d["m_wtcls"]
+                    elif sex == "F":
+                        wtcls_l = config_d["f_wtcls"]
+                    else:
+                        raise ValueError(f"Unknown sex:{sex}")
+                    for guest in [False, True]:
+                        for (i, wtcls,) in enumerate(wtcls_l):
+                            div_d = {
+                                "name": "",
+                                "gender": "",
+                                "rawOrEquipped": "",
+                                "lifts": "",
+                                "scoreBy": "",
+                                "weightClassName": wtcls,
+                                "maxWeight": "9999" if wtcls.endswith("+") else wtcls,
+                                "usaplDivisionCode": "",
+                                "hideOnBoard": ""
+                            }
+                            if i == 0:
+                                div_d.update({
+                                    "name": f"{sex} {age} {eq_d['show']} {event}{' GUEST' if guest else ''}",
+                                    "gender": "Male" if sex == "M" else "Female",
+                                    "rawOrEquipped": eq_d["lc"],
+                                    "lifts": lifts_str(event),
+                                    "scoreBy": "Total",
+                                    "usaplDivisionCode": "G" if guest else "O", #TODO - confirm this does nothing
+                                })
+                            yield div_d
+
+if __name__ == "__main__":
+    config_path = Path(sys.argv[1])
+    with open(config_path, "rt") as config_f:
+        config_d = json.load(config_f)
+    output_path = Path(sys.argv[2])
+    with open(output_path, "wt") as output_f:
+        dw = DictWriter(
+            output_f,
+            [
+                "name", "gender", "rawOrEquipped", "lifts", "scoreBy", "weightClassName",
+                "maxWeight", "usaplDivisionCode", "hideOnBoard"
+            ],
+            quoting=csv.QUOTE_ALL
+        )
+        dw.writeheader()
+        for div_row_d in gen_divisions(config_d):
+            dw.writerow(div_row_d)
