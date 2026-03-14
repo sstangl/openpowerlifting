2533e6e381501939bc99398f1b1bf4bda233f867
James Wakefield <jmw1982@gmail.com>
2026-03-14 19:48:20 +1100

    fixes to fixer script

--- scripts/fix-totals-from-checker-errors.py
+++ scripts/fix-totals-from-checker-errors.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+from csv import DictReader, DictWriter
 
 # argv[1] is path to captured checker output
 with open(sys.argv[1], "rt") as in_f:
@@ -9,33 +10,44 @@ with open(sys.argv[1], "rt") as in_f:
     for line in in_f:
         line = line.strip()
         if line.startswith("/") and line.endswith("entries.csv"):
-            entries_file = line 
+            entries_file = line
             fix_map[entries_file] = []
-        elif line.startswith("Line") and \
-            "Calculated TotalKg" in line and \
-            "but meet recorded" in line:
+        elif all((
+            line.startswith("Line"),
+            "Calculated TotalKg" in line,
+            "but meet recorded" in line,
+        )):
             fields = line.split(' ')
             fix_map[entries_file].append({
-                # enumerate() will start from 0, but the checker doesn't
-                "line_num": int(fields[1].replace(":", "")) - 1,
+                "line_num": int(fields[1].replace(":", "")),
                 "calc_total": fields[4].replace("'", "").replace(",", ""),
                 "rec_total": fields[8].replace("'", "")
             })
 for (entries_file, fixes,) in fix_map.items():
+    fixed_entries = []
+    print(f"Fixing {entries_file}")
     with open(entries_file, "rt") as entries_f:
-        fixed_entries_lines = []
-        read_entries_lines = [line.strip() for line in entries_f]
-        for (line_num, read_entries_line,) in enumerate(read_entries_lines):
-            if len(fixes) > 0 and line_num == fixes[0]["line_num"]:
-                fixed_entries_lines.append(
-                    read_entries_line.replace(
-                        fixes[0]["rec_total"],
-                        fixes[0]["calc_total"]
+        for (entry_i, entry,) in enumerate(DictReader(entries_f)):
+            # 0th entry happens on line 2
+            if len(fixes) > 0 and (entry_i + 2) == fixes[0]["line_num"]:
+                fixed_entry = dict(entry)
+                if fixed_entry["TotalKg"] != fixes[0]["rec_total"]:
+                    print(
+                        f'Expected to see {fixes[0]["rec_total"]}'
+                        f'but saw {fixed_entry["TotalKg"]} on line {entry_i}'
                     )
-                )
-                fixes.pop(0)
+                    continue
+                else:
+                    fixed_entry["TotalKg"] = fixes[0]["calc_total"]
+                    print(
+                        f'Fixed {fixed_entry["Name"]} - {fixes[0]["rec_total"]}'
+                        f'to {fixed_entry["TotalKg"]}'
+                    )
+                    fixes.pop(0)
+                    fixed_entries.append(fixed_entry)
             else:
-                fixed_entries_lines.append(read_entries_line)    
-                
+                fixed_entries.append(entry)
     with open(entries_file, "wt") as entries_f:
-        entries_f.write("\n".join(fixed_entries_lines))
+        dw = DictWriter(entries_f, fixed_entries[0].keys())
+        dw.writeheader()
+        dw.writerows(fixed_entries)
