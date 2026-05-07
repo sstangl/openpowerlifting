bc1407e1431c5e28110c9ea7b3481d409b3189bc
James Wakefield <jmw1982@gmail.com>
2026-05-07 22:11:58 +1000

    WIP: NAP conversion script

--- meet-data/nap/convert-translated-csv-from-2024.py
+++ meet-data/nap/convert-translated-csv-from-2024.py
@@ -17,7 +17,19 @@ from oplcsv import Csv
 # when we find a relevant one, produce a Csv with the entries in that section
 # and transform them based on the header and concatenate them together
 
-#TODO - functions
+def gen_section_csvs(input_csv):
+    #TODO - iterate over the rows in the input csv and yield a csv
+    # from the rows under each section header, and the section header
+    pass
+
+def make_opl_section_csv(section_csv, section_header):
+    #TODO - return a csv with the OPL schema, from the data in the section
+    # csv, and section header
+    pass
+
+def make_meet_csv():
+    #TODO - return a meet csv with the data common to all meets
+    pass
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -25,5 +37,10 @@ if __name__ == "__main__":
         sys.exit(1)
     input_path_str = sys.argv[1]
     input_csv = Csv(input_path_str)
+    entries_csv = Csv()
+    for section_csv, section_header in gen_section_csvs(input_csv):
+        section_entries_csv = make_opl_section_csv(section_csv, section_header)
+        entries_csv.cat(section_entries_csv)
+    meet_csv = make_meet_csv()
     #TODO - output entries csv filename is entries.csv in same dir
-    #TODO - write a meet.csv with blank fields as appropriate 
+    #TODO - write meet.csv in same dir
