1809d2c55463f22b7c64ca69046cc0ad27918cef
James Wakefield <jmw1982@gmail.com>
2026-05-10 09:19:36 +1000

    progress convert script

--- meet-data/nap/convert-translated-csv-from-2024.py
+++ meet-data/nap/convert-translated-csv-from-2024.py
@@ -17,10 +17,33 @@ from oplcsv import Csv
 # when we find a relevant one, produce a Csv with the entries in that section
 # and transform them based on the header and concatenate them together
 
+def get_section_header(input_csv_row):
+    # section headers have field 1 populated and no others
+    for input_field_i, input_field in enumerate(input_csv_row):
+        if input_field_i == 1 and input_field.strip() == "":
+            return False
+        if input_field_i != 1 and input_field.strip() != "":
+            return False 
+    return input_csv_row[1].strip()
+
 def gen_section_csvs(input_csv):
-    #TODO - iterate over the rows in the input csv and yield a csv
+    # iterate over the rows in the input csv and yield a csv
     # from the rows under each section header, and the section header
-    pass
+    # skip past the first two rows, which is meet data
+    include_section = False
+    for input_row in input_csv.rows[2:]:
+        section_header = get_section_header(input_row)
+        if section_header:
+            if "Press bench" in section_header or "Bench press" in section_header:
+                include_section = True
+                #TODO record other details from section header
+            if "Squat" in section_header:
+                #TODO record other details from section header
+            #TODO other headers
+        elif include_section:
+            #TODO we're including this section, make a csv out of it, then yield it
+
+
 
 def make_opl_section_csv(section_csv, section_header):
     #TODO - return a csv with the OPL schema, from the data in the section
@@ -28,8 +51,16 @@ def make_opl_section_csv(section_csv, section_header):
     pass
 
 def make_meet_csv():
-    #TODO - return a meet csv with the data common to all meets
-    pass
+    # return a meet csv with the data common to all meets.  The rest will need
+    # to be filled in manually from original_translated.csv
+    meet_csv = Csv()
+    meet_csv.append_columns([
+        "Federation","Date","MeetCountry","MeetState","MeetTown","MeetName"
+    ])
+    meet_csv.rows.append([
+        "NAP","","Russia","","",""
+    ])
+    return meet_csv
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
